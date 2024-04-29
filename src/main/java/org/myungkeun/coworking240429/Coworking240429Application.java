5f2dd2373f3271c7e1bc69a1d0002243962e38ee
myungkeun <audrms3568@gmail.com>
2024-04-29 21:30:36 +0900

    connect database

---
+++ src/main/java/org/myungkeun/coworking240429/Coworking240429Application.java
@@ -0,0 +1,13 @@
+package org.myungkeun.coworking240429;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coworking240429Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Coworking240429Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/myungkeun/coworking240429/Coworking240429ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.myungkeun.coworking240429;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Coworking240429ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
